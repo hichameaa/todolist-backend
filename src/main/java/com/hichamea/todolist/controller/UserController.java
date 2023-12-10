b1e938df4334332630206de66cd0f7d18b5ec215
Hicham Alaoui <e.alaoui.hicham@gmail.com>
2023-12-10 15:10:07 +0100

    Implemented CRUD methods for the user

--- src/main/java/com/hichamea/todolist/controller/UserController.java
+++ src/main/java/com/hichamea/todolist/controller/UserController.java
@@ -1,11 +1,86 @@
 package com.hichamea.todolist.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.hichamea.todolist.dto.UserDto;
+import com.hichamea.todolist.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Controller class for managing user-related operations.
  * This class handles operations related to user management.
  */
 @RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * Constructor for the UserController.
+     *
+     * @param userService The UserService responsible for user-related operations.
+     */
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * Retrieves a list of all users.
+     *
+     * @return ResponseEntity with a list of UserDto and HTTP status.
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        return new ResponseEntity<>(userService.findAllUsers(), HttpStatus.OK);
+    }
+
+    /**
+     * Retrieves user information by ID.
+     *
+     * @param userId The ID of the user to retrieve.
+     * @return ResponseEntity with UserDto and HTTP status.
+     */
+    @GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> getUser(@PathVariable Long userId) {
+        return new ResponseEntity<>(userService.findUserById(userId), HttpStatus.OK);
+    }
+
+    /**
+     * Endpoint for creating a new user.
+     *
+     * @param user The UserDto containing data for creating a new user.
+     * @return ResponseEntity with UserDto and HTTP status.
+     */
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
+        return new ResponseEntity<>(userService.saveUser(user), HttpStatus.CREATED);
+    }
+
+    /**
+     * Endpoint for updating an existing user.
+     *
+     * @param userId The ID of the user to update.
+     * @param user   The UserDto containing updated data.
+     * @return ResponseEntity with UserDto and HTTP status.
+     */
+    @PutMapping(value = "/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDto> updateUser(@PathVariable Long userId, @RequestBody UserDto user) {
+        return new ResponseEntity<>(userService.updateUser(userId, user), HttpStatus.CREATED);
+    }
+
+    /**
+     * Endpoint for deleting a user by ID.
+     *
+     * @param userId The ID of the user to delete.
+     * @return ResponseEntity with HTTP status.
+     */
+    @DeleteMapping(value = "/{userId}")
+    public ResponseEntity<UserDto> deleteUser(@PathVariable Long userId) {
+        userService.deleteUserById(userId);
+        return ResponseEntity.noContent().build();
+    }
 }
---
+++ src/main/java/com/hichamea/todolist/validator/UserValidator.java
@@ -0,0 +1,58 @@
+package com.hichamea.todolist.validator;
+
+import com.hichamea.todolist.dto.UserDto;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * Validation class for UserDto objects.
+ */
+public class UserValidator {
+
+    /**
+     * Validates the required fields of the UserDto object.
+     *
+     * @param user The UserDto object to validate.
+     * @return A list of error messages, or an empty list if validation succeeds.
+     */
+    public static List<String> validateUserFields(UserDto user) {
+        List<String> errors = new ArrayList<>();
+
+        // Check if the UserDto object is null
+        if (user == null) {
+            errors.add("User object is null");
+            return errors;
+        }
+
+        // Validate the required fields of the user
+        errors.addAll(Stream.of(
+                                    validateRequiredField("First name", user.getFirstName()),
+                                    validateRequiredField("Last name", user.getLastName()),
+                                    validateRequiredField("User name", user.getUserName()),
+                                    validateRequiredField("User Email", user.getEmail()),
+                                    validateRequiredField("User Password", user.getPassword())
+                            )
+                            .filter(Objects::nonNull)
+                            .toList());
+
+        return errors;
+    }
+
+    /**
+     * Validates that a required field is not empty.
+     *
+     * @param fieldName The name of the field to validate.
+     * @param value     The value of the field.
+     * @return An error message if the field is empty, otherwise null.
+     */
+    private static String validateRequiredField(String fieldName, String value) {
+        if (StringUtils.isEmpty(value)) {
+            return fieldName + " cannot be empty";
+        }
+        return null;
+    }
+}
